314fd2d81c224bc28ed6768bd70f607422e32f27
houda ben abdellaziz <houda.abdellaziz@gmail.com>
2023-06-04 16:32:10 +0100

    fix userService problem

--- src/main/java/com/example/eventmanagement/Repositories/UserRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.eventmanagement.Repositories;
 import com.example.eventmanagement.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
