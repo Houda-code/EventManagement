2b428ce5ef471b530a00305aad7b74e3a713c2f9
MediouniAmina <amina.mediouni@esprit.tn>
2023-06-05 21:36:23 +0100

    Ajouter Expensesdetails et financialReport

---
+++ src/main/java/com/example/eventmanagement/Entities/ExpensesType.java
@@ -0,0 +1,4 @@
+package com.example.eventmanagement.Entities;
+
+public enum ExpensesType {
+}
---
+++ src/main/java/com/example/eventmanagement/Entities/FinancialReport.java
@@ -0,0 +1,14 @@
+package com.example.eventmanagement.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+@Entity
+public class FinancialReport implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer ReportId;
+    private float TotalPrices;
+}
