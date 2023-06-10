ede3dc6303fab9b09176e3050691f35f3235d833
MediouniAmina <amina.mediouni@esprit.tn>
2023-06-10 12:16:14 +0100

    update2

--- src/main/java/com/example/eventmanagement/Controller/FinancialReportController.java
+++ src/main/java/com/example/eventmanagement/Controller/FinancialReportController.java
@@ -8,38 +8,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/financialReports")
 public class FinancialReportController {
-        private final FinancialReportRepository reportRepository;
 
-        @Autowired
-        public FinancialReportController(FinancialReportRepository reportRepository) {
-            this.reportRepository = reportRepository;
-        }
-
-        @GetMapping
-        public List<FinancialReport> getAllReports() {
-            return reportRepository.findAll();
-        }
-
-        @GetMapping("/{ReportI}")
-        public FinancialReport getReportById(@PathVariable("ReportId") Integer reportId) {
-            return reportRepository.findById(reportId).orElse(null);
-        }
-
-        @PostMapping
-        public FinancialReport createReport(@RequestBody FinancialReport report) {
-            return reportRepository.save(report);
-        }
-
-    //   @PutMapping("/{id}")
-      //  public FinancialReport updateReport(@PathVariable("ReportId") Integer ReportId, @RequestBody FinancialReport report) {
-        //   report.setReportId(ReportId);
-          // return reportRepository.save(report);
-        //}
-
-        @DeleteMapping("/{id}")
-        public void deleteReport(@PathVariable("id") Integer id) {
-            reportRepository.deleteById(id);
-        }
     }
 
 
