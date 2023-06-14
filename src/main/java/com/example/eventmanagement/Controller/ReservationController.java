5b465114656b6a682a8f495a2e848cae19b18215
Nadine Cherif <cherifnadine919@gmail.com>
2023-06-14 13:58:44 +0000

    ADD Reservation

--- src/main/java/com/example/eventmanagement/Controller/ReservationController.java
+++ src/main/java/com/example/eventmanagement/Controller/ReservationController.java
@@ -1,4 +1,23 @@
 package com.example.eventmanagement.Controller;
 
+import com.example.eventmanagement.Entities.Reservation;
+import com.example.eventmanagement.Repositories.ReservationRepository;
+import com.example.eventmanagement.Services.ReservationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/Resservation")
 public class ReservationController {
+    ReservationService reservationService;
+    public final ReservationRepository reservationRepository;
+    @PostMapping("/add-Reservation")
+    public Reservation addReservation(@RequestBody Reservation reservation){
+      return  reservationRepository.save(reservation);
+    }
+
 }
--- src/main/java/com/example/eventmanagement/Repositories/ReservationRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/ReservationRepository.java
@@ -5,5 +5,6 @@ import com.example.eventmanagement.Entities.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
+
 }
 
