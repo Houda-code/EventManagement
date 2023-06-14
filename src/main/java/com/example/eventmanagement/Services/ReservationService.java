05a8f81a4b84920aa5ac376128db7bf64826d8fc
Nadine Cherif <cherifnadine919@gmail.com>
2023-06-14 14:23:27 +0000

    Create Retrieve Reservation

--- src/main/java/com/example/eventmanagement/Services/ReservationService.java
+++ src/main/java/com/example/eventmanagement/Services/ReservationService.java
@@ -1,6 +1,11 @@
 package com.example.eventmanagement.Services;
 import com.example.eventmanagement.Entities.Reservation;
 
+import java.util.List;
+
 public interface ReservationService {
-    Reservation addReservation (Reservation reservation) ;
+    Reservation addReservation(Reservation reservation);
+
+    List<Reservation> RetrieveAllReservations();
+
 }
