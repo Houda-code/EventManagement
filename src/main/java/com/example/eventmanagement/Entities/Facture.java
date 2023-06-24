b9d34fde7f7feedebb4ceb8a7bc25cc70b8fa4da
Nadine Cherif <cherifnadine919@gmail.com>
2023-06-24 13:12:45 +0000

    modifeded

--- src/main/java/com/example/eventmanagement/Entities/Facture.java
+++ src/main/java/com/example/eventmanagement/Entities/Facture.java
@@ -8,13 +8,11 @@ public class Facture implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer Idfacture;
 
-<<<<<<< HEAD
-=======
+
     @ManyToOne
     Evenement event;
 
 
     public void setIdfacture(Integer idfacture) {
     }
->>>>>>> 255fc19a8e2721a0c49422200de44af950acc3ce
 }
