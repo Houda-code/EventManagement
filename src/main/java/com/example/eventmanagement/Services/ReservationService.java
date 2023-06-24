72481ca907f1c45e2850ab11a2f07c8d49e4a056
eyasoussia <eya.soussia@esprit.tn>
2023-06-24 14:44:56 +0100

    supprime reservation

--- src/main/java/com/example/eventmanagement/Services/ReservationService.java
+++ src/main/java/com/example/eventmanagement/Services/ReservationService.java
@@ -4,12 +4,6 @@ import com.example.eventmanagement.Entities.Reservation;
 import java.util.List;
 
 public interface ReservationService {
-<<<<<<< HEAD
-    Reservation addReservation (Reservation reservation) ;
-=======
-    Reservation addReservation(Reservation reservation);
 
-    List<Reservation> RetrieveAllReservations();
->>>>>>> 06453090ff229003cff54a6a5478ee73609f51b2
 
 }
