9af89474c2cb8594e489b36050f5bbe88d22b56b
chaimak1609 <chaima.kacem@esprit.tn>
2023-06-27 22:58:55 +0200

    test commit

--- src/main/java/com/example/eventmanagement/Repositories/CalendarRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/CalendarRepository.java
@@ -1,4 +1,8 @@
 package com.example.eventmanagement.Repositories;
 
-public interface CalendarRepository {
+import com.example.eventmanagement.Entities.Calendar;
+import com.example.eventmanagement.Entities.FinancialReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalendarRepository extends JpaRepository<Calendar,Integer> {
 }
--- src/main/java/com/example/eventmanagement/Repositories/PaymentRepository.java
+++ src/main/java/com/example/eventmanagement/Repositories/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.example.eventmanagement.Repositories;
 
+import com.example.eventmanagement.Entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<PaymentRepository,Integer> {
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
 }
