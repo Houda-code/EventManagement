9f5284c34dbb30190b103fa4e317b60145ad20d8
houda ben abdellaziz <houda.abdellaziz@gmail.com>
2023-06-30 17:35:10 +0100

    user crud

--- src/main/java/com/example/eventmanagement/Entities/User.java
+++ src/main/java/com/example/eventmanagement/Entities/User.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class User implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Integer id;
+  private Integer UserId;
   private String UserName;
   @Enumerated(EnumType.STRING)
   private Role role;
