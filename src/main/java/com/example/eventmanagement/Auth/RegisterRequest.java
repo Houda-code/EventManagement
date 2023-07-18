daf41d05ee7fa407118ae98d73a4acd610ff2685
Houda-code <83830601+Houda-code@users.noreply.github.com>
2023-07-18 20:53:59 +0100

    Add files via upload

---
+++ src/main/java/com/example/eventmanagement/Auth/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.example.eventmanagement.Auth;
+
+
+import com.example.eventmanagement.Entities.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Integer numTel;
+  private Role role;
+}
